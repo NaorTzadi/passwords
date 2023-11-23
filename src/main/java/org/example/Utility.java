6e17009ce3816d0ef8bc758ec0c48bac2479796a
NaorTzadi <118543436+NaorTzadi@users.noreply.github.com>
2023-11-23 08:10:16 +0200

    Update Utility.java

--- src/main/java/org/example/Utility.java
+++ src/main/java/org/example/Utility.java
@@ -6,31 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utility {
-    public static List<String> generateAll4DigitPasswords() {
-        List<String> passwords = new ArrayList<>();
 
-        for (int i = 0; i < 10000; i++) {
-            String password = String.format("%04d", i);
+    public static List<String> generatePasswordsOfLength(int length) {
+        List<String> passwords = new ArrayList<>();
+        int maxNumber = (int) Math.pow(10, length);
+
+        for (int i = 0; i < maxNumber; i++) {
+            String formatString = "%0" + length + "d";
+            String password = String.format(formatString, i);
             passwords.add(password);
         }
+
         return passwords;
-    }
-    public static String hashPassword(String password) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(password.getBytes());
-            byte[] digest = md.digest();
-            BigInteger number = new BigInteger(1, digest);
-            StringBuilder hexString = new StringBuilder(number.toString(16));
-
-            // Pad with leading zeros if necessary
-            while (hexString.length() < 64) {
-                hexString.insert(0, '0');
-            }
-
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("SHA-256 hashing algorithm not found", e);
-        }
     }
 }
