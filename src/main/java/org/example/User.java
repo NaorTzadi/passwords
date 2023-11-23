77621383ad9ce9b2c07f7ab9f7bf5489861562f7
NaorTzadi <118543436+NaorTzadi@users.noreply.github.com>
2023-11-23 08:09:57 +0200

    Update User.java

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -13,4 +13,7 @@ public class User {
     public String getUsername() {return username;}
     public String getHashedPassword() {return hashedPassword;}
     public Byte[] getSalt() {return salt;}
+    public void setUsername(String username) {this.username = username;}
+    public void setHashedPassword(String hashedPassword) {this.hashedPassword = hashedPassword;}
+    public void setSalt(Byte[] salt) {this.salt = salt;}
 }
